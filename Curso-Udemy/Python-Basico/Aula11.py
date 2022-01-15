1f2cc89d3015f1bafbacbd7582d2ec567e10bcb4
kelseykcm <kcm_unix@yahoo.com.br>
2022-01-15 10:43:38 -0300

    Exercicios-curso

--- Curso-Udemy/Python-Basico/Aula11.py
+++ Curso-Udemy/Python-Basico/Aula11.py
@@ -1,11 +1,39 @@
-"""
-A palavra pass serve pra deixar codigo para serem escritos posteriromente
+import re
 
-"""
+def is_float(val):
+    if isinstance(val,float): return True
+    if re.search(r'^\-{,1}[0-9]+\.{1}[0-9]+$', val):return True
+    return False
 
-valor = True
+def is_int(val):
+    if isinstance(val,int): return True
+    if re.search(r'^\-{,1}[0-9]+$', val): return False
+    return False
 
-if valor:
-    pass
+def is_number(val):
+    return is_int(val) or is_float(val)
+
+n1 =input("Digite um numero : ")
+n2 =input("Digite um numero : ")
+
+if is_number(n1) and is_number(n2):
+    n1 = float(n1)
+    n2 = float(n2)
+
+    print(n1 + n2)
 else:
-    print("falso")
+    print("ERRO")
+
+"""
+n1 =input("Digite um numero : ")
+n2 =input("Digite um numero : ")
+
+try:
+    n1 = float(n1)
+    n2 = float(n2)
+
+    print(n1 + n2)
+except:
+    print("ERRO")
+
+"""
---
+++ Curso-Udemy/Python-Basico/Aula12.py
@@ -0,0 +1,11 @@
+"""
+A palavra pass serve pra deixar codigo para serem escritos posteriromente
+
+"""
+
+valor = True
+
+if valor:
+    pass
+else:
+    print("falso")
